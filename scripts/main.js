82b5fd4a8752304d34193c559e5b0a6de5f76e5e
Aggiovato <aggiovato@gmail.com>
2024-09-29 13:39:32 +0200

    Commit 2: Some changes in styling and popup messages (html and css)

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ loadSection("header", "components/header.html");
 loadSection("introduction", "components/introduction.html");
 loadSection("tools", "components/tools.html");
 loadSection("buttons", "components/buttons.html");
-loadSection("code", "components/code.html");
+loadSection("css", "components/css.html");
 loadSection("modal", "components/modal.html");
 loadSection("footer", "components/footer.html");
 
@@ -20,13 +20,9 @@ function loadSection(section_id, file_path) {
 
       const paragraphs = document.querySelectorAll(".introduction p");
       const lists = document.querySelectorAll(".tools li");
-      const html_code = document.querySelectorAll(".pre-html code");
-      const css_code = document.querySelectorAll(".pre-css code");
 
       copyToClipboard(paragraphs);
       copyToClipboard(lists);
-      copyToClipboard(html_code);
-      copyToClipboard(css_code);
     })
     .catch((error) => console.log("Error al cargar el archivo:", error));
 }
@@ -61,29 +57,14 @@ function showHTML() {
   document.querySelector(".close").onclick = function () {
     modal.classList.remove("show-modal");
   };
-
-  /*window.onclick = function (event) {
-    if (event.target === modal) {
-      modal.classList.remove("show-modal");
-    }
-  };*/
 }
 
 function showCSS() {
-  let html_code = document.getElementById("html-code");
-  let css_code = document.getElementById("css-code");
-  if (
-    html_code.style.display === "block" &&
-    css_code.style.display === "none"
-  ) {
-    css_code.style.display = "block";
-    html_code.style.display = "none";
-  } else if (
-    html_code.style.display === "none" &&
-    css_code.style.display === "none"
-  ) {
-    css_code.style.display = "block";
-  } else {
-    css_code.style.display = "none";
-  }
+  let modal = document.getElementById("css-content");
+
+  modal.classList.add("show-modal");
+
+  document.querySelector(".close").onclick = function () {
+    modal.classList.remove("show-modal");
+  };
 }
